469b7bf442578c29e58d053b77e8f9a3bbf30709
divya <divu333@gmail.com>
2019-03-19 11:16:47 -0600

    finish

---
+++ js/mainCtrl.js
@@ -0,0 +1,11 @@
+angular.module("friendsList").controller("mainCtrl", function($scope) {
+  $scope.friends = [
+    "divya",
+    "kichu",
+    "deepa",
+    "amma",
+    "achan",
+    "sidhu",
+    "sanvi"
+  ];
+});
